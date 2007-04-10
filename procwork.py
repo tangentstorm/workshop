024a15bb41a91095ea55552ed447f1d057fe2615
Michal Wallace <michal.wallace@gmail.com>
2007-04-10 06:29:10 +0000

    initial checkin: async process management
    git-svn-id: file:///svn/svn/workshop/trunk@50 61d06728-c00a-0410-8f5f-eb3ba328dcb0

---
+++ procwork.py
@@ -0,0 +1,25 @@
+
+import time
+import subprocess
+from clockwork import Return, Control
+
+def waitfor(cmd, secondsToWait=0):
+    # allow strings or series of strings:
+    if type(cmd) is str and " " in cmd:
+        cmd = cmd.split()
+
+    try:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1)
+    except Exception, e:
+        raise Exception("error on %s: %s" % (cmd, str(e)))
+
+    p.killed = False
+    start = time.time()
+    while p.poll() is None:
+        if secondsToWait and (time.time() - start > secondsToWait):
+            p.killed = True
+            yield waitfor('kill -KILL %s' % p.pid)
+            break
+        else:
+            yield Control
+    yield Return(p)
