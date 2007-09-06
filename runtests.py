ac5b05b33aeb3b4655353020d13069f8da0f76b9
Michal Wallace <michal.wallace@gmail.com>
2007-09-06 21:40:30 +0000

    initial checkin
    git-svn-id: file:///svn/svn/workshop/trunk@68 61d06728-c00a-0410-8f5f-eb3ba328dcb0

---
+++ runtests.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python2.5
+from strongbox import *
+from clerks import *
+from zebra import *
+from weblib import *
+from handy import *
+from storage import *
+
+import unittest
+unittest.main()
