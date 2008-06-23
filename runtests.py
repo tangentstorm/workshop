838315f7a5d4fbf7285973b611271f74d0efef15
Michal Wallace <michal.wallace@gmail.com>
2008-06-23 14:59:47 +0000

    run all .spy tests
    git-svn-id: file:///svn/svn/workshop/trunk@91 61d06728-c00a-0410-8f5f-eb3ba328dcb0

--- runtests.py
+++ runtests.py
@@ -5,7 +5,18 @@ from zebra import *
 from weblib import *
 from handy import *
 from storage import *
+from pytypes import *
 
-import sys
+
+import os,sys
+
+
+__name__="runtests" # to prevent other unittests from running first
+spies = []
+for filename in os.listdir('.'):
+    if filename.endswith(".spy"):
+        print "loading", filename
+        exec (open(filename).read())
+        
 import unittest
 unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout))
