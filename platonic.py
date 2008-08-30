ef25456494bb89b1913b66dac8dd7c679eb6f938
Michal Wallace <michal.wallace@gmail.com>
2008-08-30 21:12:05 +0000

    REST/URI, plus lazydict and callWithKeys
    git-svn-id: file:///svn/svn/workshop/trunk@99 61d06728-c00a-0410-8f5f-eb3ba328dcb0

--- platonic.py
+++ platonic.py
@@ -1,4 +1,6 @@
-from inspect import ismethod, isfunction, getargs
+import warnings
+from types import LambdaType
+import re
 
 class Model(dict):
     
@@ -19,7 +21,41 @@ class Intercept(Exception):
          self.data = {"error":error}
 
 
+
+class lazydict(dict):
+    """
+    A lazy loading dictionary. All values
+    must be wrapped with (lambda : value)
+
+    >>> data = dict(x=5)
+    >>> lazy = lazydict( x = lambda: data['x']*2)
+    >>> lazy['x']
+    10
+    >>> data['x']=100
+    >>> lazy['x']
+    200
+    >>> lazy['y'] = lambda: lazy['x'] +1
+    >>> lazy['y']
+    201
+    >>> data['x']=12
+    >>> lazy['x']
+    24
+    >>> lazy['y']
+    25
+    
+    """
+
+    def __setitem__(self, key, wrapped):
+        assert isinstance(wrapped, LambdaType)
+        dict.__setitem__(self, key, wrapped)
+
+    def __getitem__(self, key):
+        return dict.__getitem__(self, key)()
+         
+
+
 # same as inspect.getargspec but adds support for __call__
+from inspect import ismethod, isfunction, getargs
 def getargspec(func):
     """Get the names and default values of a function's arguments.
 
@@ -58,11 +94,90 @@ def signature(f):
     return need, defaults
 
 
-def first_match(arg, *dicts):
+def firstMatch(arg, *dicts):
     for d in dicts:
         if d.has_key(arg):
             return d[arg]
     raise TypeError("missing parameter: %s" % arg)
+
+
+def callWithKeys(f, *dicts):
+    expected, defaults = signature(f)
+
+    if expected[0]=="self":
+        # @TODO: work even without convention?
+        # (could check whether it's method/bound method vs function)
+        expected.remove("self")
+        
+    return f(*[firstMatch(arg, *list(dicts)+[defaults])
+               for arg in expected])
+
+
+class URI(object):
+    def __init__(self, path, **methodHandlers):
+        self.path = path
+        self.re = re.compile(path)
+        
+        self.methods = {}
+        for k,v in methodHandlers.items():
+            self.methods[k.upper()] = v
+
+    def match(self, path):
+        return self.re.match(path)
+
+    def supports(self, method):
+        return (method.upper() in self.methods)
+        
+
+class REST(object):
+    """
+    RESTful mapping of (method, path) to handler objects.
+
+    Paths are defined as regular expressions and may
+    contain named groups.
+
+    No assumptions are made concerning how the handler
+    objects behave. This is merely a collection class.
+
+    usage:
+
+    >>> def getPerson(): pass # whatever you wante
+    >>> r = REST(URI('/person/(?P<name>.*)',
+    ...              GET=getPerson,
+    ...              PUT='put'))
+    >>> r('GET','/person/fred')
+    (<function getPerson at ...>, {'name': 'fred'})
+    >>> r.put('/person/wanda')
+    ('put', {'name': 'wanda'})
+
+    By default, the r.meth(path) syntax converts 'meth' to
+    uppercase:  r('METH', path)
+    
+    You can disable this by setting r.autoup to False.
+    """
+    def __init__(self, *uris):
+        self.uris = uris
+        self.autoup = True
+
+    def __call__(self, meth, path):
+        """
+        return (handler, pathvars) -> (object, dict)
+        """
+        for uri in self.uris:
+            match = uri.match(path)
+            if match:
+                if uri.supports(meth):
+                    return (uri.methods[meth], match.groupdict())
+                else:
+                    raise NotImplementedError('405 Method Not Allowed')
+            else: pass 
+        else:
+            # @TODO: Status 404: Not Found
+            raise LookupError("404 Not Found : %s %s" % (meth, path))
+
+    def __getattr__(self, meth):
+        return lambda(path):\
+               self(meth.upper() if self.autoup else meth, path)
 
 
 
@@ -83,18 +198,9 @@ class AbstractApp(object):
 
 
     def buildArgs(self, f, *dicts):
-
-        expected, defaults = signature(f)
-
-        if expected[0]=="self":
-             # @TODO: work even without convention?
-             # (could check whether it's method/bound method vs function)
-            expected.remove("self")
-
-        return [first_match(arg, *list(dicts)+[defaults])
-                for arg in expected]
-
-
+        warnings.warn("use platonic.callWithKeys", DeprecationWarning)
+        return callWithKeys(f, *dicts)
+        
     def dispatch(self, req, res):
 
         feature = self.buildFeature(req)
