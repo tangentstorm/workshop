603b060f6cd206766bfc8c218305e51fac1851f9
Michal Wallace <michal.wallace@gmail.com>
2008-12-20 10:04:16 +0000

    arlo as universal combinator language
    git-svn-id: file:///svn/svn/workshop/trunk@130 61d06728-c00a-0410-8f5f-eb3ba328dcb0

--- code/arlo.py
+++ code/arlo.py
@@ -1,67 +1,155 @@
+from warnings import warn
+import sys
 
-class QueryExpression(object):
-    OPS = {
-        "&"         : "AND",
-        "|"         : "OR",
-        "startswith": "LIKE",
-        "endswith"  : "LIKE",
-    }
-    
-    def __init__(self, left, right, operation):
+# a -> Bool
+def wrapped(value):
+    return isinstance(value, Expr)
+
+
+binaryOps = dict(
+    EQ='==', NE="!=", LT="<", GT=">", LE="<=", GE=">=",
+    AND='&', OR="|", XOR="^", LSHIFT="<<", RSHIFT=">>",
+    ADD="+", SUB="-", MUL="*", DIV="/", MOD="%",
+    POW="**", FLOORDIV='//')
+
+
+unaryOps = dict(
+    NEG="-", POS="+", INVERT="~")
+
+
+def declare(s):
+    xs = s.split()
+    return "%s = %s" % (','.join(xs), ",".join('_.%s' % x for x in xs))
+assert declare("a b c") == "a,b,c = _.a,_.b,_.c"
+
+
+# data Expr |
+
+class Expr(object):
+
+    def __init__(self, left, op, right):
         self.left = left
         self.right = right
-        self.operation = operation
-        #@TODO: remove this!
-        self.pattern = (left, right, operation)
+        self.op = op
 
+    def _wrap(self, other, wrapper):
+        if type(other) == tuple: return tuple(self._wrap(x,wrapper) for x in other)
+        else: return other if wrapped(other) else wrapper(other)
+
+    # (bool, str) -> Expr
+    def _build(self, op, other):
+        return Expr(self, op, self._wrap(other, Const))
     
     def __and__(self, other):
-        return QueryExpression(self, other, '&')
+        return Expr(self, '&', other)
 
     def __or__(self, other):
-        return QueryExpression(self, other, '|')
+        return Expr(self, '|', other)
+
+    def __repr__(self):
+        return '(%r %s %r)' % (self.left, self.op, self.right)
 
     def __str__(self):
-        format = isinstance(self.right, QueryExpression) and u'(%s %s %s)' or u"(%s %s '%s')" 
-        right = self.operation == "startswith" and u"%s%%" % self.right or self.right
-        right = self.operation == "endswith" and u"%%%s" % right or right
-        op = self.OPS.get(self.operation, self.operation)
-        return format % (self.left, op, right)
+        return '(%s %s %s)' % (self.left, self.op, self.right)
+
+    def __call__(self, *others, **kw):
+        if kw: warn("keyword arguments don't work yet! (%s)" % kw)
+        return CallExpr(self, tuple(self._wrap(other, Const) for other in others))
+
+    def __getattr__(self, other):
+        return DotExpr(self, self._wrap(other, Name))
+
+    def __getitem__(self, other):
+        return SubExpr(self, self._wrap(other, Name))
+
+
+## build the magic method combinators ################################
+
+# Op Str , Sym str , arity -> (QueryExpr, QueryExpr -> QueryExpr)
+def _makeMagic(op, sym, arity=2):
+    if arity == 1:
+        pass # @TODO: magic Unary operators
+    else:
+        def magic(self, other):
+            return self._build(sym, other)
+    return magic
+        
+
+for op, sym in binaryOps.items():
+    magicMethod = "__%s__" % op.lower()
+    if not hasattr(Expr, magicMethod):
+        setattr(Expr, magicMethod, _makeMagic(op, sym, 2))
 
 
 
+## special constructs #################################################
 
-
-class QueryBuilder(object):
-
-    def __init__(self, name=''):
-        self._name = name
-
-    def __eq__(self, other):
-        return QueryExpression(self._name, other, '=') 
-
-    def __ne__(self, other):
-        return QueryExpression(self._name, other, '!=') 
-
-    def __lt__(self, other):
-        return QueryExpression(self._name, other, '<') 
-    
-    def __gt__(self, other):
-        return QueryExpression(self._name, other, '>')
-
-    def __ge__(self, other):
-        return QueryExpression(self._name, other, '>=')
-    
-    def __le__(self, other):
-        return QueryExpression(self._name, other, '<=')
-
-    def startswith(self, other):
-        return QueryExpression(self._name, other, 'startswith') 
-
-    def endswith(self, other):
-        return QueryExpression(self._name, other, 'endswith') 
-
+# | DotExpr Expr Expr
+class DotExpr(Expr):
+    def __init__(self, left, right):
+        return super(DotExpr,self).__init__(left, '.', Name(right))
+    def __repr__(self):
+        return '%r.%r' % (self.left, self.right)
     def __str__(self):
-        return self._name
+        if isinstance(self.left, Arlo):
+            return str(self.right)
+        else:
+            return '%s.%s' % (self.left, self.right)
 
-where = QueryBuilder
+# | CallExpr Expr Expr
+class CallExpr(Expr):
+    def __init__(self, left, right):
+        assert type(right) == tuple, 'CallExpr wants tuple, got %s' % type(right)
+        return super(CallExpr,self).__init__(left, '()', right)
+    def __repr__(self):
+        return '%r(%s)' % (self.left, ','.join(repr(s) for s in self.right))
+    def __str__(self):
+        return '%s(%s)' % (self.left, ','.join(str(s) for s in self.right))
+
+# | SubExpr Expr Expr
+class SubExpr(Expr):
+    def __init__(self, left, right):
+        return super(SubExpr,self).__init__(left, '[]', right)
+    def __repr__(self):
+        right = self.right if type(self.right) == tuple else (self.right,)
+        return '%r[%r]' % (self.left, ','.join(repr(s) for s in right))
+    def __str__(self):
+        right = self.right if type(self.right) == tuple else (self.right,)        
+        return '%s[%s]' % (self.left, ','.join(str(s) for s in right))
+
+
+# Str -> Const Str
+class Const(Expr):
+    def __init__(self, _):
+        self._ = _
+    def __str__(self):
+        return repr(self._)
+    def __repr__(self):
+        return "_(%r)" % self._
+
+
+# Str -> Nameifier Str
+class Name(Const):
+    def __str__(self):
+        return str(self._)
+    def __repr__(self):
+        return "%s" % self._
+
+
+## builders #########################################
+
+class Arlo(Expr):
+    def __init__(self):
+        pass
+    def __getattr__(self, o):
+        return DotExpr(self, Name(o))
+    def __call__(self, o):
+        return Const(o)
+    def __str__(self):
+        return "_"
+    def __repr__(self):
+        return "_"
+    
+
+_ = Arlo()
+
